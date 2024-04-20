c792fabeb66b946127119abb63cf5c1bd25fde76
Kamiko Loss <info@gloxi.net>
2024-04-21 02:17:01 +0900

    スローが完了する前にリトライするとスローが解除されない不具合を修正した

--- scenes/game.gd
+++ scenes/game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 # Scenes
 const GATE_SCENE = preload("res://scenes/gate.tscn")
+const ENEMY_SCENE = preload("res://scenes/enemy.tscn")
 
 # Resources
 # TODO
@@ -17,7 +18,9 @@ const GATE_HEIGHT_MIN = -80 # (px)
 const GATE_HEIGHT_MAX = 80 # (px) 
 
 # Variables
-var _gate_spawn_cooltime = 2 # 何秒ごとにゲートを生成するか
+var _slow_tween = null
+var _gate_spawn_cooltime = 2.0 # 何秒ごとにゲートを生成するか
+var _enemy_spawn_cooltime = 3.0 # 何秒ごとに敵を生成するか
 var _rng = RandomNumberGenerator.new() # 乱数生成
 
 
@@ -49,9 +52,11 @@ func _initialize_game():
 	Global.initialize()
 	Global.game_initialized.emit()
 
-	# ゲート生成開始
+
+	# ゲートおよび敵の生成開始
 	# Global.init() のあとに呼ぶこと
 	_loop_spawn_gate()
+	_loop_spawn_enemy()
 
 
 func _on_ui_jumped():
@@ -106,31 +111,35 @@ func _calc_score():
 	return Global.level * Global.money * Global.extra
 
 
+# スロー用の Tween を取得する
+func _get_slow_tween():
+	if (_slow_tween):
+		_slow_tween.kill()
+
+	_slow_tween = create_tween()
+	_slow_tween.set_trans(Tween.TRANS_QUINT)
+	_slow_tween.set_ease(Tween.EASE_OUT)
+	return _slow_tween
+
+
 # 通常速度からスローになっていく
 func _enter_slow():
-	var _tween = get_tree().create_tween()
-	_tween.set_trans(Tween.TRANS_CIRC)
-	_tween.set_ease(Tween.EASE_OUT)
-	_tween.tween_property(Engine, "time_scale", SLOW_SPEED, SLOW_DURATION)
+	_get_slow_tween().tween_property(Engine, "time_scale", SLOW_SPEED, SLOW_DURATION)
 
 
 # スローから通常速度になっていく
 func _leave_slow():
-	var _tween = get_tree().create_tween()
-	_tween.set_trans(Tween.TRANS_CIRC)
-	_tween.set_ease(Tween.EASE_OUT)
-	_tween.tween_property(Engine, "time_scale", 1.0, SLOW_DURATION)
+	_get_slow_tween().tween_property(Engine, "time_scale", 1.0, SLOW_DURATION)
 
 
 # ゲートを生成する
 func _spawn_gate():
 	print("Gate is spawned.")
-	var _new_gate = GATE_SCENE.instantiate()
+	var _gate = GATE_SCENE.instantiate()
 	var _height_diff = _rng.randf_range(GATE_HEIGHT_MIN, GATE_HEIGHT_MAX)
-	_new_gate.position.x += (_hero.position.x + 360)
-	_new_gate.position.y +=  _height_diff
-	get_tree().root.get_node("Main").add_child(_new_gate)
-	return _new_gate
+	_gate.position.x += (_hero.position.x + 360)
+	_gate.position.y += _height_diff
+	get_tree().root.get_node("Main").add_child(_gate)
 
 
 # ゲートを生成しつづける
@@ -148,3 +157,30 @@ func _loop_spawn_gate():
 	# 一定時間待ったあと次のループに移行する
 	await get_tree().create_timer(_gate_spawn_cooltime).timeout
 	_loop_spawn_gate()
+
+
+# 敵を生成する
+func _spawn_enemy():
+	print("Enemy is spawned.")
+	var _enemy = ENEMY_SCENE.instantiate()
+	var _height_diff = _rng.randf_range(GATE_HEIGHT_MIN, GATE_HEIGHT_MAX)
+	_enemy.position.x += (_hero.position.x + 360)
+	_enemy.position.y += (_height_diff + 320)
+	get_tree().root.get_node("Main").add_child(_enemy)
+
+
+# 敵を生成しつづける
+# TODO: pause 対応 (await じゃなくてループ内でやる？)
+func _loop_spawn_enemy():
+	# ゲームオーバー: ループを中止する
+	if (Global.game_state == Global.GameState.GAMEOVER):
+		print("Stop spawn enemy loop.")
+		return
+
+	# ゲーム中: 敵を生成する
+	if (Global.game_state == Global.GameState.ACTIVE):
+		_spawn_enemy()
+
+	# 一定時間待ったあと次のループに移行する
+	await get_tree().create_timer(_enemy_spawn_cooltime).timeout
+	_loop_spawn_enemy()
