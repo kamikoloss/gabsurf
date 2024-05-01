ceb60cf804827a16fc7e5217d005f4fa97edd1e3
Kamiko Loss <info@gloxi.net>
2024-05-01 23:04:48 +0900

    ミサイルの速度を Tween にした

--- scenes/weapon.gd
+++ scenes/weapon.gd
@@ -2,7 +2,18 @@ extends Area2D
 
 
 # Variables
-var speed = 300 # 飛行速度 (px/s)
+var speed = 0 # 飛行速度 (px/s)
+
+# Constants
+const SPEED_FROM = 200
+const SPEED_TO = 800
+const SPEED_DURATION = 1.0
+
+
+func _ready():
+	var _tween = create_tween()
+	_tween.set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
+	_tween.tween_method(func(v): speed = v, SPEED_FROM, SPEED_TO, SPEED_DURATION)
 
 
 func _process(delta):
