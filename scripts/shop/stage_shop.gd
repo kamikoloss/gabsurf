17d3ad54d00da2e36d121205f9c4a5a476dd8d0a
Kamiko Loss <info@gloxi.net>
2024-05-24 21:13:08 +0900

    微修正

--- scripts/shop/stage_shop.gd
+++ scripts/shop/stage_shop.gd
@@ -5,18 +5,18 @@ class_name StageShop
 
 # Stage の情報
 # "g": Gate 1枚で LEVEL がいくつ増えるか
-# "t": 名称, "d1": 説明文メイン, "d2": 説明文サブ
+# "t": 名称, "d": 説明文メイン
 var _stage_info = {
 	# Stage 1
 	Global.StageType.A: { "g": 1, "t": "ビル街", "d1": ""},
 	# Stage 2
-	Global.StageType.B: { "g": 3, "t": "B", "d1": "ショップを通過するごとに\nゲートの狭さ -8" },
-	Global.StageType.C: { "g": 2, "t": "C", "d1": "ゲートが上下に動く\n (速度: 低)" },
+	Global.StageType.B: { "g": 3, "t": "B", "d": "ショップを通過するごとに\nゲートの狭さ -8" },
+	Global.StageType.C: { "g": 2, "t": "C", "d": "ゲートが上下に動く\n (速度: 低)" },
 	# Stage 3
-	Global.StageType.D: { "g": 5, "t": "D", "d1": "ショップを通過するごとに\nゲートの狭さ -16" },
-	Global.StageType.E: { "g": 3, "t": "E", "d1": "" },
-	# Stage EX
-	Global.StageType.X: { "g": 5, "t": "成層圏", "d1": "重力 1/2", "d2": "GATE = LEVEL +5" },
+	Global.StageType.D: { "g": 5, "t": "D", "d": "ショップを通過するごとに\nゲートの狭さ -16" },
+	Global.StageType.E: { "g": 3, "t": "E", "d": "" },
+	# Stage X
+	Global.StageType.X: { "g": 5, "t": "成層圏", "d": "重力 1/2"},
 }
 
 # Stage Number ごとに店に並ぶ Stage のリスト
