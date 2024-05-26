a4a39e6f177b19ad249b1cdc623d9f1c42be3543
Kamiko Loss <info@gloxi.net>
2024-05-26 19:56:13 +0900

    ミサイルから煙の Particle が出るようにした (途中)

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -5,6 +5,8 @@ const SPEED_FROM = 200
 const SPEED_TO = 800
 const SPEED_DURATION = 1.0
 
+const DESTROY_TIME = 10 # 生まれて何秒後に自身を破壊するか
+
 
 var speed = 0 # 飛行速度 (px/s)
 
@@ -14,12 +16,15 @@ func _ready():
 	_tween.set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
 	_tween.tween_method(func(v): speed = v, SPEED_FROM, SPEED_TO, SPEED_DURATION)
 
+	_auto_destroy()
+
 
 func _process(delta):
 	position.x += speed * delta
 
 
-func _on_area_exited(area):
-	if area.is_in_group("ScreenOut"):
-		#print("[Weapon] destroyed.")
-		queue_free()
+# 指定秒数後に自身を破壊する
+func _auto_destroy():
+	await get_tree().create_timer(DESTROY_TIME).timeout
+	print("[Weapon] destroyed.")
+	queue_free()
